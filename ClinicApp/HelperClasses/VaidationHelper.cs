96f7d088e9c5ff9819199977cd81e86522a59ad4
Djaba00 <shipelenko9@gmail.com>
2023-09-13 18:25:30 +0500

    Добавьте файлы проекта.

---
+++ ClinicApp/HelperClasses/VaidationHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicApp.HelperClasses
+{
+    public static class VaidationHelper
+    {
+        public static bool IsDateOfBirthIsValid(this DateTime? dateOfBirth)
+        {
+            if (dateOfBirth >= DateTime.MinValue && dateOfBirth <= DateTime.Today)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
